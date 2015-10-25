376119e7386064d8672e2548514e3fdfc37c1d1d
Ken <348999853@qq.com>
2015-10-25 23:22:01 +0800

    modify the img reference from $service to $row when getFollowShopList

--- yaohephp/admin/Apps/Home/Controller/ApiController.class.php
+++ yaohephp/admin/Apps/Home/Controller/ApiController.class.php
@@ -1401,21 +1401,20 @@ class ApiController extends Controller {
 			if(empty($item['content']))$item['content']=	$row['content'];
 			//$item['img1']	=	$row['img1'];
 
-			if(!empty($service['img6']))$item['img']=	$service['img6'];
-			if(!empty($service['img5']))$item['img']=	$service['img5'];
-			if(!empty($service['img4']))$item['img']=	$service['img4'];
-			if(!empty($service['img3']))$item['img']=	$service['img3'];
-			if(!empty($service['img2']))$item['img']=	$service['img2'];
-			if(!empty($service['img1']))$item['img']=	$service['img1'];
-
+			if(!empty($item['img6']))$item['img']	=	$row['img6'];
+			if(!empty($item['img5']))$item['img']	=	$row['img5'];
+			if(!empty($item['img4']))$item['img']	=	$row['img4'];
+			if(!empty($item['img3']))$item['img']	=	$row['img3'];
+			if(!empty($item['img2']))$item['img']	=	$row['img2'];
+			if(!empty($item['img1']))$item['img']	=	$row['img1'];
 			if(!isset($item['img']))
 			{
-				if(!empty($item['img6']))$item['img']	=	$item['img6'];
-				if(!empty($item['img5']))$item['img']	=	$item['img5'];
-				if(!empty($item['img4']))$item['img']	=	$item['img4'];
-				if(!empty($item['img3']))$item['img']	=	$item['img3'];
-				if(!empty($item['img2']))$item['img']	=	$item['img2'];
-				if(!empty($item['img1']))$item['img']	=	$item['img1'];
+				if(!empty($service['img6']))$item['img']=	$item['img6'];
+				if(!empty($service['img5']))$item['img']=	$item['img5'];
+				if(!empty($service['img4']))$item['img']=	$item['img4'];
+				if(!empty($service['img3']))$item['img']=	$item['img3'];
+				if(!empty($service['img2']))$item['img']=	$item['img2'];
+				if(!empty($service['img1']))$item['img']=	$item['img1'];
 			}
 			if(!isset($item['img']))$item['img']='';
 			$item['img1']=$item['img'];
